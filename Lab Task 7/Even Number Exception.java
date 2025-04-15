971a896fb3617cc300e548a883facb8c6994ae20
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2025-04-16 01:56:29 +0600

    Create Even Number Exception.java

---
+++ Lab Task 7/Even Number Exception.java
@@ -0,0 +1,22 @@
+import java.util.*;
+
+public class Main {
+    public static void checkNumber(int number) throws ArithmeticException {
+        if (number % 2 == 0) {
+            throw new ArithmeticException("Even number!");
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int num = sc.nextInt();
+
+        try {
+            checkNumber(num);
+            System.out.println("Number is odd.");
+        } catch (ArithmeticException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+}

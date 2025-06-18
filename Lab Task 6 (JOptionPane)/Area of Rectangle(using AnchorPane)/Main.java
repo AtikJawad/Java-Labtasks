6f7969a3bc43ca9c6105ee1dd6995ea2e6a0e107
Atik Jawad <85114480+AtikJawad@users.noreply.github.com>
2025-06-19 01:40:33 +0600

    Create Main.java

---
+++ Lab Task 6 (JOptionPane)/Area of Rectangle(using AnchorPane)/Main.java
@@ -0,0 +1,15 @@
+
+package lab.practice;
+
+import java.util.*;
+import javax.swing.*;
+public class LabPractice {
+
+    
+   public static void main(String[] args) {
+       
+        NewJFrame N = new NewJFrame();
+        N.show();
+    }
+    
+}
